576b24deee0ed5443e1401fceae1b8f0c5e6c602
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-04-12 11:38:24 -0300

    ex02(functions): button save message, dont lose data and product page update

--- ex02/script.js
+++ ex02/script.js
@@ -1,5 +1,22 @@
 let array = [];
 
+alertMessage = () => {
+  const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+  const appendAlert = (message, type) => {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      "</div>",
+    ].join("");
+
+    alertPlaceholder.append(wrapper);
+  };
+
+  appendAlert("Produto adicionado com sucesso!", "success");
+};
+
 class Form {
   constructor() {
     this.titulo = document.getElementById("titulo");
@@ -16,17 +33,27 @@ class Form {
       quantidade: this.quantidade.value,
     };
 
-    console.log(objeto);
+    let eVazio = !Object.values(objeto).every(
+      (x) => x !== undefined && x !== ""
+    );
 
-    array.push(objeto);
-
-    let eVazio = Object.values(objeto).every((x) => x !== null && x !== "");
-
-    if (!eVazio) {
+    if (eVazio) {
       alert("Preencha todos os campos!");
       return;
     } else {
-      localStorage.setItem("array", JSON.stringify(array));
+      let dadosSalvos = localStorage.getItem("array");
+      alertMessage();
+
+      if (dadosSalvos === null || dadosSalvos === undefined) {
+        array.push(objeto);
+        localStorage.setItem("array", JSON.stringify(array));
+      } else {
+        let novaPosicao = JSON.parse(dadosSalvos);
+
+        novaPosicao.push(objeto);
+
+        localStorage.setItem("array", JSON.stringify(novaPosicao));
+      }
       return;
     }
   }
@@ -35,9 +62,16 @@ class Form {
     event.preventDefault();
     let dados = localStorage.getItem("array");
     let formProducts = JSON.parse(dados);
+    document.getElementById("divProdutos").innerHTML = "";
+    if (localStorage.array) {
+      document.getElementById("divAlert").innerHTML = "";
+    }
 
     for (let i = 0; i < formProducts.length; i++) {
-      console.log(i);
+      let divItem = document.createElement("div");
+      divItem.setAttribute("class", "div card text-bg-light mb-3");
+      divItem.setAttribute("id", `c0${i}`);
+      document.getElementById("divProdutos").appendChild(divItem);
 
       let tituloItem = document.createElement("h3");
       tituloItem.appendChild(
